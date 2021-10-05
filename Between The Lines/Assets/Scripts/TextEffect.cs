f944c7b2865051e8862c8ad98b50982a3022ffe7
MBHayes00 <mbh9947@rit.edu>
2021-10-04 23:37:42 -0400

    Added collision between the letters and the mouse cursor

--- Between The Lines/Assets/Scripts/TextEffect.cs
+++ Between The Lines/Assets/Scripts/TextEffect.cs
@@ -6,15 +6,21 @@ using TMPro;
 public class TextEffect : MonoBehaviour
 {
     TMP_Text textMesh;
+    Canvas canvas;
 
     Mesh mesh;
 
     Vector3[] vertices;
     [SerializeField] float wobbleAmplitude; //How intense should the wobble be
+
+    //Variables about mouse cursor
+    bool mouseGrabbingWord = false;
+
     // Start is called before the first frame update
     void Start()
     {
         textMesh = GetComponent<TMP_Text>();
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
     }
 
     // Update is called once per frame
@@ -23,6 +29,10 @@ public class TextEffect : MonoBehaviour
         textMesh.ForceMeshUpdate();
         mesh = textMesh.mesh;
         vertices = mesh.vertices;
+
+        float mousePosX = Input.mousePosition.x - (canvas.pixelRect.width / 2);
+        float mousePosY = Input.mousePosition.y - (canvas.pixelRect.height / 2);
+
         for (int i = 0; i < textMesh.textInfo.characterCount; i++)
         {
             TMP_CharacterInfo c = textMesh.textInfo.characterInfo[i];
@@ -30,10 +40,28 @@ public class TextEffect : MonoBehaviour
             int index = c.vertexIndex;
           
             Vector3 offset = Wobble(Time.time + i);
+            //Bottom Left
             vertices[index] += offset;
+            //Top Left
             vertices[index + 1] += offset;
+            //Top Right
             vertices[index + 2] += offset;
+            //Bottom Right
             vertices[index + 3] += offset;
+
+            //AABB collision detection
+            //using the bottom left and top right vertices
+
+            //Detecting the x axis
+            if (mousePosX > vertices[index].x && mousePosX < vertices[index + 2].x)
+            {
+                //Detecting the y axis
+                if (mousePosY > vertices[index].y && mousePosY < vertices[index + 2].y)
+                {
+                    Debug.Log("Over letter");
+                }
+            }
+
         }
 
         mesh.vertices = vertices;
