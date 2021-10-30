7340d482784ac5cda048f01333403f584788059b
JT5519 <joel.sk10@gmail.com>
2021-10-30 18:14:31 -0400

    WIP

--- Between The Lines/Assets/Scripts/AnswerScrambler.cs
+++ Between The Lines/Assets/Scripts/AnswerScrambler.cs
@@ -9,6 +9,7 @@ public class AnswerScrambler : MonoBehaviour
     [SerializeField] float wobbleAmplitude; //Wobble intensity
     MouseInteract[] letterMouseInteract; //Array of text mesh mouse interactions
 
+    Shockwave shockwaveScript;
     private void Start()
     {
         //Fill the array with text meshes' mouse interactions
@@ -18,13 +19,15 @@ public class AnswerScrambler : MonoBehaviour
         {
             letterMouseInteract[i] = textMesh[i].GetComponent<MouseInteract>();
         }
+
+        shockwaveScript = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Shockwave>();
     }
     void Update()
     {
         //loop to wobble each textmesh in the array by an offset of i in each iteration and an offset of deltaTime every frame
         for (int i = 0; i < textMesh.Length; i++)
         {
-            if(!letterMouseInteract[i].GetLetterHeldByCursor() && !letterMouseInteract[i].GetLetterInPlace())
+            if(!letterMouseInteract[i].GetLetterHeldByCursor() && !letterMouseInteract[i].GetLetterInPlace() && !inWavePath(textMesh[i]))
             {
                 Vector3 offset = Wobble(Time.time + i);
                 textMesh[i].transform.position += offset;
@@ -35,5 +38,11 @@ public class AnswerScrambler : MonoBehaviour
     //wobble generating function
     Vector2 Wobble(float time) {
         return new Vector2(wobbleAmplitude * Mathf.Sin(time*3.3f), wobbleAmplitude * Mathf.Cos(time*2.5f));
+    }
+    bool inWavePath(TMP_Text meshToCheck)
+    {
+        if (shockwaveScript.objectsShockwaveCollidesWith.Contains(meshToCheck.gameObject))
+            return true;
+        return false;
     }
 }
--- Between The Lines/Assets/Scripts/Shockwave.cs
+++ Between The Lines/Assets/Scripts/Shockwave.cs
@@ -5,60 +5,70 @@ using UnityEngine;
 public class Shockwave : MonoBehaviour
 {
     public GameObject shockwavePrefab;
+    [HideInInspector] public GameObject wave;
+    
     public float shockwaveMaxSize;
     public float shockwaveSpeed;
     public int shockwaveCooldown;
+    public float shockwaveHangTime; //how long it persists after growing to max size 
+
     private Vector3 mousePos;
-    private GameObject wave;
-    public float timer;
+    public float timerWhenWaveAbsent; //timer used when shockwave is absent
+    public float timerWhenWavePresent; //timer when shockwave is present
 
-
+    [HideInInspector] public HashSet<GameObject> objectsShockwaveCollidesWith; 
     // Start is called before the first frame update
     void Start()
     {
-
+        timerWhenWaveAbsent = 0;
+        timerWhenWavePresent = 0;
+        objectsShockwaveCollidesWith = new HashSet<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Section of code that only runs when wave does not exist
+
         //Keep up with time if a wave is not present
         if (!wave)
         {
-            timer += Time.deltaTime;
+            timerWhenWaveAbsent += Time.deltaTime;
         }
-
 
         //On Mouse Click Create New Shockwave
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1) && !wave && timerWhenWaveAbsent >= shockwaveCooldown)
         {
-            if (!wave && timer >= shockwaveCooldown)
-            {
-                //Get Mouse Pos
-                mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-                mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-                mousePos.z = 0f;
+            //Get Mouse Pos
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
-                //Spawn the Shockwave
-                wave = Instantiate(shockwavePrefab, mousePos, Quaternion.identity);
+            //Spawn the Shockwave
+            wave = Instantiate(shockwavePrefab, mousePos, Quaternion.identity);
 
 
-                timer = 0;
-            }
-
+            timerWhenWaveAbsent = 0;
         }
 
+        //Section of code that runs only when wave exists
 
         //Update the Wave if it Exists
         if (wave)
         {
-            if (wave.transform.localScale.x <= shockwaveMaxSize)            
+            if (wave.transform.localScale.x < shockwaveMaxSize)            
             {
                 wave.transform.localScale += new Vector3(Time.deltaTime * shockwaveSpeed, Time.deltaTime * shockwaveSpeed, 0);
-                wave.GetComponent<CircleCollider2D>().radius = wave.transform.localScale.x / 2;
+                //wave.GetComponent<CircleCollider2D>().radius = wave.transform.localScale.x / 2;
             }
-            else
+            else if(timerWhenWavePresent < shockwaveHangTime)
             {
+                //persist unter timer crosses hangtime
+                timerWhenWavePresent += Time.deltaTime;
+            }
+            else if(timerWhenWavePresent >= shockwaveHangTime)
+            {
+                timerWhenWavePresent = 0;
+                objectsShockwaveCollidesWith.Clear();
                 Destroy(wave);
             }
         }
